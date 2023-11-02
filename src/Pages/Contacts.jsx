22ece986233aef1e789f60b6bf5f1691230fadd5
NickTsalyn <129570585+NickTsalyn@users.noreply.github.com>
2023-11-02 11:02:47 -0400

    Update Contacts.jsx

--- src/Pages/Contacts.jsx
+++ src/Pages/Contacts.jsx
@@ -5,8 +5,17 @@ import { ContactForm } from '../components/ContactForm/ContactForm';
 import { Filter } from '../components/Filter/Filter';
 import { ContactList } from '../components/ContactList/ContactList';
 import { ContactTitle, HomeTitle } from 'components/HomeInfo/HomeInfo.styled';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchContacts } from 'redux/contacts/operations';
 
 const Contacts = () => {
+
+    const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
   return (
     <>
       <div>
